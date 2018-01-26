ace28f40fcebdfb9af346c14e77f8b424bff9f01
Chen, Kai <chenk.ck03@gmail.com>
2018-01-26 10:58:07 +0100

    day 5 poisson distribution 1

---
+++ 10_days_of_statistics/day5_poisson_distribution_1.py
@@ -0,0 +1,25 @@
+#https://www.hackerrank.com/challenges/s10-poisson-distribution-1/problem
+
+# Task
+# A random variable X, follows Poisson distribution with mean of 2.5.
+# Find the probability with which the random variable X is equal to 5.
+
+# Input Format
+# The first line contains X's mean.
+# The second line contains the value we want the probability for:
+# 2.5
+# 5
+
+# Output Format
+# Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1234 format).
+
+import math
+
+def poisson_distribution(k, lam):
+    p = math.pow(lam, k) * math.exp(-1*lam) / math.factorial(k)
+    return p
+
+
+lam = 2.5
+k = 5
+print(round(poisson_distribution(k, lam), 3))
